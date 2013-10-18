10203e26d8e43e3e889ee211a9c0528db774b395
Josua Krause <josua.krause@gmail.com>
2013-10-18 16:52:33 -0400

    curved paths

--- src/main/trails/Particle.java
+++ src/main/trails/Particle.java
@@ -1,8 +1,15 @@
 package trails;
 
+import static jkanvas.util.VecUtil.*;
+
+import java.awt.geom.Point2D;
+
 import jkanvas.animation.AnimatedPosition;
+import jkanvas.animation.GenericAnimated;
 
 public class Particle extends AnimatedPosition {
+
+  public static double bendRatio = 0.4;
 
   private final double size;
 
@@ -13,6 +20,20 @@ public class Particle extends AnimatedPosition {
 
   public double getSize() {
     return size;
+  }
+
+  @Override
+  protected GenericAnimated<Point2D> createAnimated(final Point2D pos) {
+    return new GenericAnimated<Point2D>(pos) {
+
+      @Override
+      protected Point2D interpolate(final Point2D from, final Point2D to, final double t) {
+        final Point2D diff = subVec(to, from);
+        final Point2D ref = addVec(mulVec(getOrthoRight(diff), bendRatio), to);
+        return interpolateBezier(from, ref, to, t);
+      }
+
+    };
   }
 
 }
