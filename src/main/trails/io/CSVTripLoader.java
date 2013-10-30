8e780b073ce6648d1581b04f7a16c0e876b7b3cd
Josua Krause <josua.krause@gmail.com>
2013-10-30 17:14:00 -0400

    zip crashing prevented

---
+++ src/main/trails/io/CSVTripLoader.java
@@ -0,0 +1,121 @@
+package trails.io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import jkanvas.io.csv.CSVReader;
+import jkanvas.io.csv.CSVRow;
+import jkanvas.util.Resource;
+
+public class CSVTripLoader {
+
+  private final Resource r;
+
+  private final CSVReader reader = new CSVReader(',', '"', true, false, true);
+
+  public CSVTripLoader(final Resource r) {
+    this.r = Objects.requireNonNull(r);
+  }
+
+  public void loadTrips(final File out) throws IOException {
+    final Integer[] perm = getPermutation();
+    try (RandomAccessFile outFile = new RandomAccessFile(out, "w")) {
+      outFile.setLength(Trip.byteSize() * perm.length);
+      final Iterator<CSVRow> it = openFile("trip_data_1.csv");
+      int rowNo = 0;
+      while(it.hasNext()) {
+        final CSVRow row = it.next();
+        Trip t = null;
+        // read the trip
+        try {
+          t = new Trip(
+              row.get("pickup_latitude"),
+              row.get("pickup_longitude"),
+              row.get("pickup_datetime"),
+              row.get("dropoff_latitude"),
+              row.get("dropoff_longitude"),
+              row.get("dropoff_datetime"));
+        } catch(final IllegalArgumentException e) {
+          System.err.println("invalid row detected");
+          e.printStackTrace();
+        }
+        if(t == null) {
+          t = new Trip();
+        }
+        // write the trip
+        outFile.seek(perm[rowNo] * Trip.byteSize());
+        t.write(outFile);
+        // row numbers
+        ++rowNo;
+      }
+    }
+  }
+
+  private Integer[] getPermutation() throws IOException {
+    final List<Long> times = new ArrayList<>();
+    final Iterator<CSVRow> it = openFile("trip_data_1.csv");
+    while(it.hasNext()) {
+      final CSVRow row = it.next();
+      System.out.println(row.get("pickup_datetime"));
+      final long date = Trip.parseDate(row.get("pickup_datetime"));
+      System.out.println(date);
+      times.add(date);
+    }
+    final Integer[] perm = new Integer[times.size()];
+    for(int i = 0; i < perm.length; ++i) {
+      perm[i] = i;
+    }
+    Arrays.sort(perm, new Comparator<Integer>() {
+
+      @Override
+      public int compare(final Integer o1, final Integer o2) {
+        return Long.compare(times.get(o1), times.get(o2));
+      }
+
+    });
+    return perm;
+  }
+
+  private Iterator<CSVRow> openFile(final String name) throws IOException {
+    if(r.isZip()) {
+      final ZipInputStream zip = new ZipInputStream(r.getURL().openStream());
+      boolean found = false;
+      ZipEntry cur;
+      while((cur = zip.getNextEntry()) != null) {
+        if(name.equals(cur.getName())) {
+          found = true;
+          break;
+        }
+        zip.closeEntry();
+      }
+      if(!found) {
+        zip.close();
+        throw new FileNotFoundException(name + " in " + r);
+      }
+      return CSVReader.readRows(
+          new BufferedReader( // always buffer zip inputs :)
+              new InputStreamReader(zip, r.getCharset())), reader);
+    }
+    return CSVReader.readRows(r.getFile(name).reader(), reader);
+  }
+
+  public static void main(final String[] args) throws IOException {
+    final CSVTripLoader l = new CSVTripLoader(Resource.getFor("trip_data_1.csv.zip"));
+    final Resource dump = new Resource(
+        (String) null, "trip_data_1.dat", (String) null, (String) null);
+    l.loadTrips(dump.directFile());
+  }
+
+}
--- src/main/trails/io/Trip.java
+++ src/main/trails/io/Trip.java
@@ -1,8 +1,11 @@
 package trails.io;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class Trip {
 
@@ -18,6 +21,10 @@ public class Trip {
 
   public final long dTime;
 
+  public Trip() {
+    this(Double.NaN, Double.NaN, -1L, Double.NaN, Double.NaN, -1L);
+  }
+
   public Trip(final String pickupLat, final String pickupLon, final String pickupTime,
       final String dropoffLat, final String dropoffLon, final String dropoffTime) {
     pLat = parseDouble(pickupLat);
@@ -26,6 +33,22 @@ public class Trip {
     dLon = parseDouble(dropoffLon);
     pTime = parseDate(pickupTime);
     dTime = parseDate(dropoffTime);
+  }
+
+  private Trip(final double pLat, final double pLon, final long pTime, final double dLat,
+      final double dLon, final long dTime) {
+    this.pLat = pLat;
+    this.pLon = pLon;
+    this.pTime = pTime;
+    this.dLat = dLat;
+    this.dLon = dLon;
+    this.dTime = dTime;
+  }
+
+  public boolean isValid() {
+    return !Double.isNaN(dLat) && !Double.isNaN(dLon) &&
+        !Double.isNaN(pLat) && !Double.isNaN(pLon) &&
+        pTime >= 0L && dTime >= 0L;
   }
 
   private static double parseDouble(final String d) {
@@ -37,16 +60,40 @@ public class Trip {
   }
 
   /** The date format. */
-  private static final SimpleDateFormat DATE_PARSER = new SimpleDateFormat(
-      "yyyy-MM-dd HH:mm:ss");
+  private static final String DATE_PARSER = "yyyy-MM-dd HH:mm:ss";
 
-  private static long parseDate(final String d) {
+  public static long parseDate(final String d) {
     try {
-      final Date date = DATE_PARSER.parse(d);
+      final Date date = new SimpleDateFormat(DATE_PARSER, Locale.US).parse(d);
       return date.getTime();
     } catch(ParseException | NullPointerException e) {
       throw new IllegalArgumentException(e);
     }
+  }
+
+  public void write(final RandomAccessFile out) throws IOException {
+    out.writeLong(pTime); // 8
+    out.writeLong(dTime); // 8
+    out.writeDouble(pLat); // 8
+    out.writeDouble(pLon); // 8
+    out.writeDouble(dLat); // 8
+    out.writeDouble(dLon); // 8
+    // total bytes: 48
+  }
+
+  public static Trip read(final RandomAccessFile in) throws IOException {
+    final long pTime = in.readLong(); // 8
+    final long dTime = in.readLong(); // 8
+    final double pLat = in.readDouble(); // 8
+    final double pLon = in.readDouble(); // 8
+    final double dLat = in.readDouble(); // 8
+    final double dLon = in.readDouble(); // 8
+    // total bytes: 48
+    return new Trip(pLat, pLon, pTime, dLat, dLon, dTime);
+  }
+
+  public static long byteSize() {
+    return 6 * 8;
   }
 
 }
